8e91c76d7a558e54740d60fb3363f640a483de59
Emmanuel Lopez <jesusemm010@hotmail.com>
2021-09-28 12:15:42 -0500

    Ya hay esta conectado a IndexedDB

---
+++ js/app.js
@@ -0,0 +1,36 @@
+(function(){
+    let DB; 
+    document.addEventListener('DOMContentLoaded', function(){ 
+        crearDB();
+        
+    })
+
+    function crearDB(){
+
+        const crearDB = window.indexedDB.open("crm", 1);
+    
+        crearDB.onerror = () =>{
+            console.log("Hubo un error");
+        }
+    
+        crearDB.onsuccess = function(){
+            DB = crearDB.result; //Abrir la conexion
+        }
+    
+        crearDB.onupgradeneeded = function (e){
+            const db = e.target.result;
+            const objectStore = db.createObjectStore("crm", {keyPath: "id", autoIncrement: true});
+    
+            objectStore.createIndex("nombre", "nombre", {unique: false});
+            objectStore.createIndex("email", "email", {unique: true});
+            objectStore.createIndex("telefono", "telefono", {unique: false});
+            objectStore.createIndex("empresa", "empresa", {unique: false});
+            objectStore.createIndex("id", "id", {unique: true});
+    
+            console.log("lista y creada");
+        }
+    }
+
+})();
+
+//CREA LA BASE DE DATOS
