8b5633ff4de277f027f8a3bae43cfc74f7f864f1
Emmanuel Lopez <jesusemm010@hotmail.com>
2021-09-29 18:45:38 -0500

    El cliente se agrega a la base de datos

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -8,10 +8,10 @@
     });
 
     function conectarDB() {
-        const abrirConexion = window.indexedDB.open(["crm"], 1);
+        const abrirConexion = window.indexedDB.open("crm", 1);
 
         abrirConexion.onerror = function ( ){
-            console.log("error");
+            imprimirAlerta("hubo un error", "error");
         }
         abrirConexion.onsuccess = function ( ){
             DB = abrirConexion.result;
@@ -27,9 +27,39 @@
         const telefono = document.querySelector("#telefono").value;
         const empresa = document.querySelector("#empresa").value;
 
-        if(nombre === "" || email === "" || telefono === "" || emepresa === ""){
+        if(nombre === "" || email === "" || telefono === "" || empresa === ""){
             imprimirAlerta("Todos los campos son obligatorios", "error");
             return
+        }
+
+        //Crear un obejto con la informacion 
+        const cliente = {
+            nombre,
+            email, 
+            telefono,
+            empresa,
+        };
+        cliente.id = Date.now();
+
+        crearNuevoCliente(cliente);
+
+    }
+
+    function crearNuevoCliente(cliente){
+        const transaction = DB.transaction(["crm"], "readwrite");
+        const objectStore = transaction.objectStore("crm");
+
+        objectStore.add(cliente);
+
+        transaction.onerror = function () {
+            imprimirAlerta("hubo un error", "error");
+        }
+        transaction.oncomplete = function () {
+            imprimirAlerta("Cliente agregado correctamente") ; 
+
+            setTimeout( () =>{
+                window.location.href  = "index.html";
+            }, 1000)
         }
     }
 
