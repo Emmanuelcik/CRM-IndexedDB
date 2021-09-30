cdb6348e03641dad6ead30154d51a3b523519a4d
Emmanuel Lopez <jesusemm010@hotmail.com>
2021-09-29 19:05:25 -0500

    Se listan todos los clientes en el index

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@
     let DB; 
     document.addEventListener('DOMContentLoaded', function(){ 
         crearDB();
-        
+        if(window.indexedDB.open("crm", 1)){
+            obtenerClientes();
+        }
     })
 
     function crearDB(){
@@ -31,6 +33,49 @@
         }
     }
 
+    function obtenerClientes() {
+        //Abrir conexion 
+        const abrirConexion = window.indexedDB.open("crm", 1);
+        
+        abrirConexion.onerror = function () {
+            console.log("error");
+        }
+
+        abrirConexion.onsuccess = function () {
+            DB = abrirConexion.result;
+            const objectStore = DB.transaction("crm").objectStore("crm");
+
+            objectStore.openCursor().onsuccess = function (e) {
+                const cursor = e.target.result;
+                if(cursor){
+                    const {nombre, empresa, email, telefono, id} = cursor.value;
+
+                    const listadoClientes = document.querySelector("#listado-clientes");
+                    listadoClientes.innerHTML +=  ` 
+                    <tr>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                            <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
+                            <p class="text-sm leading-10 text-gray-700"> ${email} </p>
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
+                            <p class="text-gray-700">${telefono}</p>
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
+                            <p class="text-gray-600">${empresa}</p>
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+                            <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
+                            <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                        </td>
+                    </tr>`;
+
+                    cursor.continue();
+                }else{
+                    console.log("no hay mas");
+                }
+            }
+        }
+    }
 })();
 
-//CREA LA BASE DE DATOS
+
